f3d69a21a10ce9bfc38b68d72a138c68c40af658
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-14 17:15:43 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,25 @@
 # code your #valid_move? method here
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def valid_move?(board, index)
+  if(0<=index && index<=8 && (board[index]== "" || board[index]==" "  || board[index] == nil))
+    return true
+  
+    
+    else
+    return false
+  end
+  end
+  
+  def position_taken?(board, index)
+  if(board[index] == "" || board[index] == " " || board[index]=nil)
+    return false
+  
+  
+  else 
+    return true
+  end
+end
+  
+
