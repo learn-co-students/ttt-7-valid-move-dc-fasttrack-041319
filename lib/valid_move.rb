22d3ae8295072ae3512f43d9c75db94a89d50fec
Athena Salomons <athena.asalomons@gmail.com>
2019-04-14 14:31:01 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,18 @@
 # code your #valid_move? method here
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board [0]
+board [1]
+board [2]
+board [3]
+board [4]
+board [5]
+board [6]
+board [7]
+board [8]
 
-
+if board.between(0,8)
+  puts board
+else
+  return "Enter a valid number."
+  
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
